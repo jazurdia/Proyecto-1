15ca695521b151951e01a9e029f8a699f836ec12
Diego2250 <mor21146@uvg.edu.gt>
2022-03-23 00:08:43 -0600

    Update Setvar.java
    Funcionando por el momento

--- Clases/Setvar.java
+++ Clases/Setvar.java
@@ -1,5 +1,27 @@
 package Clases;
-
+import java.util.HashMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 public class Setvar {
-
+    private HashMap<String, Integer> myVars;
+    public Setvar(){
+        myVars= new HashMap<String, Integer>(); 
+    }
+    public void SetVar(String expresion) {
+        Pattern pattern = Pattern.compile("[ ]+[a-z]+[ ]+", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(expresion);
+        String Nombre = "";
+	    Integer Valor = 0; 
+        if (matcher.find()) {
+            Nombre = matcher.group();
+        }
+        pattern = Pattern.compile("[ ]+[0-9]+[ ]*", Pattern.CASE_INSENSITIVE); //
+        matcher = pattern.matcher(expresion);
+        if (matcher.find()) {
+            Valor = Integer.parseInt(matcher.group().trim());
+        }
+        myVars.put(Nombre, Valor);
+        System.out.println("Variable: " + Nombre + " asignada con valor " + Valor);
+    }
+    
 }
