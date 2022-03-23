61aab52d337a68535806aa9b2111270648d6c064
Diego2250 <mor21146@uvg.edu.gt>
2022-03-23 12:00:20 -0600

    Update ScannerSintax.java

--- Clases/ScannerSintax.java
+++ Clases/ScannerSintax.java
@@ -7,14 +7,14 @@ public class ScannerSintax {
 			return 1;
 		else if (evaluate("^[(]{1}[+-*/]{1} [0-9.]+ [0-9.]+[)]{1}$",expresion)) //This is a simple add operation of 2 operands
 			return 2;
-		else if (evaluate("^[(]{1}[<>]{1}[=]{0,1} [0-9.]+ [0-9.]+[)]{1}$", expresion))	
+		else if (evaluate("^[(]{1}[<>]{1}[=]{0,1} [$0-9.]+ [0-9.]+[)]{1}$", expresion))	
 			return 3; 
-		else if (evaluate("^[(]{1}[+-*/]{1} [A-z]+ [A-z]+[)]{1}$", expresion)); 
-			return 4;
-		else if (evaluate("^[(]{1}[<>]{1}[=]{0,1} [A-z]+ [A-z]+[)]{1}$", expresion));
-		else{
+		else if (evaluate("^[(]{1}[+-*/]{1} [A-z]+ [A-z]+[)]{1}$",expresion))	
+			return 4; 
+		else if (evaluate("^[(]{1}[<>]{1}[=]{0,1} [A-z]+ [A-z]+[)]{1}$", expresion))
+			return 5;	
+		else 
 			return -1; 
-		} 	 
     }
 
     private static boolean evaluate(String regex, String expresion) {
