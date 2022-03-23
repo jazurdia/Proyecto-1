fb86aa9f9d453d5243bbdc0e52c013c5d1d53697
Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>
2022-03-23 10:26:06 -0600

    Update Predicados.java

--- Clases/Predicados.java
+++ Clases/Predicados.java
@@ -12,7 +12,7 @@ public class Predicados {
      * @param a Primer número a comparar
      * @param b Segundo número a comparar
      * @return boolean
-     */
+     *//
     public boolean isEqual(double a, double b){
         boolean ans = false;
         if(a == b){
@@ -27,7 +27,7 @@ public class Predicados {
      * @param a Primer número a comparar
      * @param b Segundo número a comparar
      * @return boolean
-     */
+     *//
     public boolean isGreater(double a, double b){
         boolean ans = false;
         if(a < b){
@@ -41,7 +41,7 @@ public class Predicados {
      * @param a Primer número a comparar
      * @param b Segundo número a comparar
      * @return boolean
-     */
+     *//
     public boolean isLesser(double a, double b){
         boolean ans = false;
         if(a < b){
