a145ada52a5603ffddffb173b930e020232d6cc9
Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>
2022-03-23 14:56:12 -0600

    update

--- Clases/Predicados.java
+++ Clases/Predicados.java
@@ -5,6 +5,8 @@
 package Clases;
 public class Predicados {
 
+    SepararString sep = new SepararString();
+
     public Predicados(){}
 
     /**
@@ -49,5 +51,30 @@ public class Predicados {
         }
         return ans;
     }
+
+    public boolean comparar(String string){
+        boolean ans = false;
+        String operacion_string[] = sep.separar(string);
+        String signo = operacion_string[0];
+        double num1 = sep.stringToDouble(operacion_string[1]);
+        double num2 = sep.stringToDouble(operacion_string[2]);
+
+        switch (signo) {
+            case "==": //equals
+                ans = isEqual(num1, num2);     
+                break;
+
+            case "<": //isLesser
+                ans = isLesser(num1, num2);     
+                break;
+
+            case ">": //isGreater
+                ans = isGreater(num1, num2);
+                break;
+        }
+
+        return ans;
+    }
+
 
 }
