5213ccae05fc375c5ec177c6f823f2cc9ce5f305
Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>
2022-03-23 10:29:44 -0600

    Update Aritmetica.java

--- Clases/Aritmetica.java
+++ Clases/Aritmetica.java
@@ -5,36 +5,6 @@
 package Clases;
 
 public class Aritmetica {
-    public Aritmetica() {
-    }
-
-    public double execute_aritmetica(double numero_a, double numero_b, String operacion){
-        double ans;
-
-        switch (operacion) {
-            case "+":
-                ans = numero_a + numero_b;
-                break;
-        
-            case "-":
-                ans = numero_a - numero_b;
-                break;
-
-            case "*":
-                ans = numero_a * numero_b;
-                break;
-
-            case "/":
-                ans = numero_a / numero_b;
-                break;
-
-            default:
-                ans = 0;
-                break;
-
-        }
-        
-        return ans;
-    }
+    
 
 }
