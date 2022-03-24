085e214a4f4cc7174f641640d5ffde76ae7f4e0d
Diego2250 <mor21146@uvg.edu.gt>
2022-03-23 22:13:27 -0600

    Update vista y controlador

--- Clases/Main.java
+++ Clases/Main.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Interprete miLispInterpreter = new Interprete();
+        Interprete LispInterpreter = new Interprete();
         Vista v = new Vista();
         Scanner in = new Scanner(System.in);
         v.bienvenida();
-        System.out.println("Bienvenido al interpreter Lisp ingrese una expresion o escriba exit para salir");
+        v.print("Ingrese una expresion o escriba 'exit' para salir");
         String opt = "";
-        opt = in.nextLine();
-        miLispInterpreter.Operar(opt);
+        do {
+            opt = in.nextLine();
+            if (!opt.toUpperCase().equals("EXIT")){
+                LispInterpreter.Operar(opt);
+            }else{
+                v.despedida();
+            }
+        } while (!opt.toUpperCase().equals("EXIT"));
 
     }
 }
--- Clases/Vista.java
+++ Clases/Vista.java
@@ -14,42 +14,8 @@ public class Vista {
         System.out.println("Gracias por utilizar el interprete de Lisp");
     }
 
-    public int menuOpcionesInicio(){
-        int op=0;
-        try {
-            String s= "¿Que deseas hacer?\n" +
-                        "1. Ingresar expresión\n" +
-                        "2. Salir\n";
-            System.out.println(s);
-            op = scan.nextInt();            
-        } catch (Exception e) {
-            error();
-            scan.next();
-        }
-        return op; 
-    }
-
-    public int menuOpcionesExpresiones(){
-        int op=0;
-        try {
-            String s= "¿Que deseas ingresar?\n" +
-                        "1. Operación aritmética\n" +
-                        "2. Funcion\n";
-            System.out.println(s);
-            op = scan.nextInt();            
-        } catch (Exception e) {
-            error();
-            scan.next();
-        }
-        return op; 
-    }
-
     public void error(){
 		System.out.println("Ha ocurrido un error");
-	}
-
-    public void opcionInvalida(){
-		System.out.println("Has elegido una opción inválida.");
 	}
 
     public void print(String string){
