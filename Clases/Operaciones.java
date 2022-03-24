48112b3289d2e6e0c66534755360cd1216616a98
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-03-23 19:17:41 -0600

    Update Operaciones.java

--- Clases/Operaciones.java
+++ Clases/Operaciones.java
@@ -4,18 +4,20 @@ public class Operaciones {
     private Aritmetica aritmetica = new Aritmetica();
     private Predicados predicados = new Predicados();
     private SepararString sep = new SepararString();
-    public Operaciones(){}
 
-    public String operar(String instrucciones){
+    public Operaciones() {
+    }
+
+    public String operar(String instrucciones) {
         String ans = "";
 
         String instrucciones_array[] = sep.separar(instrucciones);
         String signo = instrucciones_array[0];
-        if(signo == "+"|| signo == "-" || signo == "*" || signo == "/"){
-            double temp = aritmetica.execute_aritmetica(instrucciones)
+        if (signo == "+" || signo == "-" || signo == "*" || signo == "/") {
+            double temp = aritmetica.execute_aritmetica(instrucciones);
             ans = String.valueOf(temp);
-    
-        }else ( signo == "=="|| signo == "<" || signo == ">"){
+
+        } else if (signo == "==" || signo == "<" || signo == ">") {
             boolean temp = predicados.comparar(instrucciones);
             ans = String.valueOf(temp);
         }
