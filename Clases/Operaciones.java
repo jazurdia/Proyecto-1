5a28f46a5052a2e0959259c4e2fd4ee17474273b
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-03-23 19:50:52 -0600

    Update Operaciones.java

--- Clases/Operaciones.java
+++ Clases/Operaciones.java
@@ -13,14 +13,26 @@ public class Operaciones {
 
         String instrucciones_array[] = sep.separar(instrucciones);
         String signo = instrucciones_array[0];
+        
+        /*
         if (signo == "+" || signo == "-" || signo == "*" || signo == "/") {
             double temp = aritmetica.execute_aritmetica(instrucciones);
-            ans = String.valueOf(temp);
+            ans = String.valueOf(temp); //""+""
 
         } else if (signo == "==" || signo == "<" || signo == ">") {
             boolean temp = predicados.comparar(instrucciones);
             ans = String.valueOf(temp);
         }
+        */
+
+        if(signo.equals("+") || signo.equals("-")|| signo.equals("*") ||signo.equals("/")) {
+            double temp = aritmetica.execute_aritmetica(instrucciones);
+            ans = String.valueOf(temp); //""+""
+        }else if(signo.equals("==")||signo.equals("<")||signo.equals("*")||signo.equals(">")){
+            boolean temp = predicados.comparar(instrucciones);
+            ans = String.valueOf(temp);
+        }
+        
         return ans;
     }
 }
